3543444aa927585b83fc1bcf19da1a32bdc038aa
ing <ingafter60@outlook.com>
2021-07-04 23:44:59 +0700

    2. Setup static files, templates and homepage

--- README.md
+++ README.md
@@ -3,4 +3,16 @@ This is my exercise building ecommerce application based on the tutorial made by
 
 #### 1. Setup - Create venv, install django create project and app
 
+#### 2. Setup static files, templates and homepage
+
+        modified:   README.md
+        new file:   apps/home/urls.py
+        modified:   apps/home/views.py
+        modified:   config/settings.py
+        modified:   config/urls.py
+        ...
+        new file:   static/js/main.js
+        new file:   static/js/price-range.js
+        new file:   templates/home/index.html
+
 
--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os 
 
 from pathlib import Path
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,9 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# NEW
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
